2388bb0d9d68b7f38c750b76e662701b6f51f936
Mac <mac@Macs-iMac.local>
2017-08-08 00:17:21 +0530

    first commit

---
+++ CustomDelegate/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CustomDelegate
+//
+//  Created by Mac on 07/08/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
